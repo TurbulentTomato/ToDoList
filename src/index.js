858018738dba745ebefb89507068702fbc393cc5
TurbulentTomato <abhinavgangwar87@gmail.com>
2024-12-02 10:35:37 +0530

    Create ProjectList
    ProjectList is an IIFE that allows user to add to and get the array
    containing all the projects

---
+++ src/index.js
@@ -0,0 +1,22 @@
+//initial plan : each project will have classifications
+//called topic or list, user should be able to add new list/topic,
+//in each topic/list, user can create to-Dos
+//note: each topic/list will be an object having its title
+//and an array containing its to-Dos
+const ProjectList = (function() {
+  let projects = [];
+  const addProject = (title) => {
+    //creates and pushes a Prpject object that contains
+    //title of project and an array to store its topics/lists
+    projects.push({ title, list: [] });
+  }
+  const getList = () => {
+    console.log(projects);
+    return projects;
+  }
+  return { addProject, getList };
+})();
+
+//attaching functions and other things to window
+//so i can use them in dev-tools console
+window.ProjectList = ProjectList;
