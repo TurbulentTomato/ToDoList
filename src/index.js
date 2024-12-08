b9fe22666c224edd666b32c6a2d2e7fd6f7d49b8
TurbulentTomato <abhinavgangwar87@gmail.com>
2024-12-08 21:05:11 +0530

    Add DomHandler, bindEvents method, event listeners
    Added event listeners to open and close modals

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
 //note: each topic/list will be an object having its title
 //and an array containing its to-Dos
 import { ProjectList, toDoCreator, UtilityHandler, RenderHandler } from "./barrelModule";
+import "./reset.css";
+import "./style.css";
 (() => {
   let list = ProjectList.getList();
   if (localStorage.getItem("projects")) {
@@ -14,6 +16,36 @@ import { ProjectList, toDoCreator, UtilityHandler, RenderHandler } from "./barre
   ProjectList.addProject("Home"); //for some weird reason,this does not work when written without function and breaks the code
 })()
 
+const DomHandler = (function() {
+  const sidebar = document.querySelector("#sidebar");
+  const projectContainer = document.querySelector(".project-list");
+  const listContainer = document.querySelector("#list-container");
+  const addTaskBtn = document.querySelector("#add-task-btn");
+  const addListBtn = document.querySelector("#add-list-btn");
+  const addProjectBtn = document.querySelector("#add-project-btn");
+  const cancelBtn = document.querySelectorAll(".cancel-btn");
+  const addProjectModal = document.querySelector(".add-project-modal");
+  const addListModal = document.querySelector(".add-list-modal");
+  const addTaskModal = document.querySelector(".add-task-modal");
+  const bindEvents = () => {
+    addTaskBtn.addEventListener("click", () => { addTaskModal.showModal() })
+    addProjectBtn.addEventListener("click", (event) => {
+      addProjectModal.showModal();
+    })
+    addListBtn.addEventListener("click", (event) => {
+      addListModal.showModal();
+    })
+    cancelBtn.forEach(button => {
+      button.addEventListener("click", () => {
+        addProjectModal.close();
+        addListModal.close();
+        addTaskModal.close();
+      })
+    })
+  }
+  return { bindEvents }
+})();
+DomHandler.bindEvents();
 //attaching functions and other things to window
 //so i can use them in dev-tools console
 window.ProjectList = ProjectList;
