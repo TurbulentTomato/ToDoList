bdb7c625d8b84131b54dff7c5b44c5557f50ce09
TurbulentTomato <abhinavgangwar87@gmail.com>
2024-12-31 11:10:09 +0530

    Merge renderProject,ProjectList,List into render

--- src/renderHandler.js
+++ src/renderHandler.js
@@ -1,26 +1,9 @@
 export const RenderHandler = (function() {
   //will currently log to console
   let output = "";
-  const renderProjectList = (container, addBtn, list) => {
-    //output = `Available projects:${ProjectList.getList().map(project => " " + project.title)}`;
-    // console.log(output);
+  const render = (container, addBtn, list) => {
     container.innerHTML = list;
     container.prepend(addBtn);
-  }
-  const renderProject = (container, addBtn, list) => {
-    /*  output = `
-  Project: ${project.title}
-  Available Lists:${project.listCollection.map(list => { return " " + list.title })}
-  `;
-      console.log(output);*/
-    container.innerHTML = list;
-    container.prepend(addBtn);
-  }
-  const renderList = (container, addBtn, list) => {
-    /* output = `List: ${list.title}\n\n`;
-     renderToDos(list);*/
-    container.innerHTML = list;
-    container.prepend(addBtn)
   }
   const renderToDos = (container, list) => {
     /*  for (let toDo of list.toDos) {
@@ -54,10 +37,10 @@ export const RenderHandler = (function() {
             list: element
           }, renderingImportant);
         }
-        renderList(container, addBtn, innerHtml)
+        render(container, addBtn, innerHtml)
       });
       renderingImportant = renderingImportant ? false : null;
     } while (renderingImportant != null)
   }
-  return { renderProjectList, renderProject, renderList, renderToDos, renderAllToDos }
+  return { render, renderToDos, renderAllToDos }
 })();
